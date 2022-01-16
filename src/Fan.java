12b2c9e83ed6a17dd069361ad8a5210b3a2c258d
Kiu-Kyu <minkiukiu@gmail.com>
2022-01-17 03:47:37 +0700

    add 1 file

---
+++ src/Fan.java
@@ -0,0 +1,65 @@
+public class Fan {
+    public static final int SLOW = 1;
+    public static final int MEDIUM = 2;
+    public static final int FAST = 3;
+
+    private int speed;
+    private boolean on;
+    private double radius;
+    private String color;
+
+    public Fan() {
+        this.speed = SLOW;
+        this.on = false;
+        this.radius = 5;
+        this.color = "blue";
+    }
+
+    public Fan(int speed, boolean on, double radius, String color) {
+        this.speed = speed;
+        this.on = on;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        if (this.on) {
+            return "Speed: " + this.speed + " color: " + this.color + " radius: " + this.radius;
+        } else {
+            return "Fan is off. Color: " + this.color + " radius: " + this.radius;
+        }
+    }
+}
